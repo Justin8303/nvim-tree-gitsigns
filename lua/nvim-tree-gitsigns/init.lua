a177c569d2fbee1a06c304c4dfbe55121d845a3c
Justin Wittwer <justin.wittwer@gmx.de>
2025-06-08 12:21:19 +0200

    moved init.lua to correct location

--- lua/nvim-tree-gitsigns/init.lua
+++ lua/nvim-tree-gitsigns/init.lua
@@ -1,8 +1,43 @@
 local M = {}
 
-function M.setup()
-  local function sync_nvimtree_gitsigns_colors()
-    local git_map = {
+function M.setup_tree()
+  local gs_config = package.loaded['gitsigns.config'] and package.loaded['gitsigns.config'].config or {}
+  local signs = gs_config.signs or {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked = { text = '+' },
+  }
+
+  local git_glyphs = {
+    unstaged  = signs.change and signs.change.text or '~',
+    staged    = signs.add and signs.add.text or '+',
+    unmerged  = signs.changedelete and signs.changedelete.text or '~',
+    renamed   = signs.change and signs.change.text or '~',
+    untracked = signs.untracked and signs.untracked.text or (signs.add and signs.add.text or '+'),
+    deleted   = signs.delete and signs.delete.text or '_',
+    ignored   = "◌",
+  }
+
+  require("nvim-tree").setup {
+    renderer = {
+      group_empty = true,
+      icons = {
+        glyphs = {
+          git = git_glyphs,
+        },
+      }
+    },
+    sort = { sorter = "case_sensitive" },
+    view = { width = 36 },
+    filters = { dotfiles = true },
+  }
+end
+
+function M.setup_colors()
+local git_map = {
       Staged    = "GitSignsAdd",
       Untracked = "GitSignsUntracked",
       Deleted   = "GitSignsDelete",
@@ -20,15 +55,20 @@ function M.setup()
     end
     local ok, api = pcall(require, "nvim-tree.api")
     if ok then api.tree.reload() end
-  end
 
-  sync_nvimtree_gitsigns_colors()
+end
 
+function M.post_setup()
+  M.setup_tree()
+  M.setup_colors()
+end
+
+function M.setup()
   vim.api.nvim_create_autocmd("ColorScheme", {
     group = vim.api.nvim_create_augroup("NvimTreeGitColors", { clear = true }),
     pattern = "*",
     callback = function()
-      vim.defer_fn(sync_nvimtree_gitsigns_colors, 100)
+      vim.defer_fn(M.post_setup, 100)
     end,
   })
 end
